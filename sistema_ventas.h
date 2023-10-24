7e0119c9cba75124b673f8fa7c296e161c770f30
Arturo Rodriguez Contreras <arturo211234@gmail.com>
2023-10-24 09:56:50 -0600

    cambiar a formato binario

--- sistema_ventas.h
+++ sistema_ventas.h
@@ -1,15 +1,14 @@
 #ifndef SISTEMA_VENTAS_H
 #include <string.h>
 #include <stdio.h>
-#include <stdlib.h> 
+#include <stdlib.h>
 #define SISTEMA_VENTAS_H 1
 
 typedef struct articulo_t{
- char nombreArticulo[64]; 
+ char nombreArticulo[64];
  float precio;
+ int cantidad;
 }articulo_t;
-
-articulo_t leerArticulo(char * nom, float pre);
 
 void menuVentas(articulo_t * articulos, int numArt);
 
