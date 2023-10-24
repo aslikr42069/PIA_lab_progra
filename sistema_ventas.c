2f300baea313ab75cc2a35164f920f9045da3270
Arturo Rodriguez Contreras <arturo211234@gmail.com>
2023-10-24 09:57:14 -0600

    cambiar a formato binario

--- sistema_ventas.c
+++ sistema_ventas.c
@@ -1,11 +1,5 @@
 #include "sistema_ventas.h"
 
-articulo_t leerArticulo(char * nom, float pre){
- articulo_t art;
- strcpy(art.nombreArticulo, nom);
- art.precio = pre;
- return art;
-}
 
 
 void menuVentas(articulo_t * articulos, int numArt){
